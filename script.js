2210f0087ad301f7a17d5e56db62bb8e5e3e82ec
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2021-02-10 19:16:20 +0100

    apiroot changed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-   const apiRoot = 'https://whispering-oasis-09032.herokuapp.com/v1/task/';
-   const trelloApiRoot = 'https://whispering-oasis-09032.herokuapp.com/v1/task/';
+   const apiRoot = 'https://whispering-oasis-09032.herokuapp.com/v1/task';
+   const trelloApiRoot = 'https://whispering-oasis-09032.herokuapp.com/v1/trello';
    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
    const $tasksContainer = $('[data-tasks-container]');
 
